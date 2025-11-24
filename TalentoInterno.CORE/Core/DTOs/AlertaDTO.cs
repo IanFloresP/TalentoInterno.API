bc4f350a1371fb7fd6d4f814b7ff1589bc69f0da
alessandroandrade2601 <alessandro.andrade.2601@gmail.com>
2025-11-24 11:06:57 -0500

    Cambio AlertaDTO y AduitoriaResumenDTO

--- TalentoInterno.CORE/Core/DTOs/AlertaDTO.cs
+++ TalentoInterno.CORE/Core/DTOs/AlertaDTO.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
+﻿namespace TalentoInterno.CORE.Core.DTOs;
 
-namespace TalentoInterno.CORE.Core.DTOs;
-
-public class AlertaDTO
+public class AlertaDto
 {
-    public string Tipo { get; set; } = null!;
-    public string Severidad { get; set; } = null!;
+    public string Tipo { get; set; } = null!; // "RIESGO_VACANTE" o "ESCASEZ_SKILL"
     public string Mensaje { get; set; } = null!;
-    public string Detalles { get; set; } = null!;
-    public List<string> Sugerencias { get; set; } = new();
+    public string Criticidad { get; set; }  // Alta, Media, Baja
+    public int? EntidadId { get; set; } // ID de la Vacante o Skill
+    public string EntidadNombre { get; set; } = null!; // Nombre de la Vacante o Skill
+    public int ValorActual { get; set; } // Ej: 0 candidatos
 }
